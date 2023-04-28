a2b4714a78156d0240e955fd16bcf9b4a44debb5
GordonOtieno <otienogordon95@gmail.com>
2023-04-28 21:41:52 +0300

    Update api endpoints routes

--- routes/api.php
+++ routes/api.php
@@ -1,19 +1,18 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\Api\V1\AuthController;
+use App\Http\Controllers\Api\V1\UserController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
-
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+// Route::middleware('auth:sanctum')->group(function(){
+//     Route::get('inside-mware', function(){
+//         return response()->json('success', 200);
+//     });
+// });
+Route::group(['prefix'=>'v1'],function(){
+  Route::post('/auth/register',[AuthController::class,'register']);
+  Route::post('/auth/login',[AuthController::class,'login']);
+  Route::get('/users/{id}',[UserController::class,'show']);
+  Route::put('/users/{id}',[UserController::class,'update'])->middleware('auth:sanctum');
+  Route::post('/auth/logout',[AuthController::class,'logout'])->middleware('auth:sanctum');
 });

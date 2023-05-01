82757aee616537763a589657ee3a06ee8eb3b07b
GordonOtieno <otienogordon95@gmail.com>
2023-05-01 20:45:34 +0300

    Update Route file add show and update user endpoints

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,9 @@
 <?php
 
-use App\Http\Controllers\Api\V1\AuthController;
-use App\Http\Controllers\Api\V1\UserController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\V1\AuthController;
+use App\Http\Controllers\Api\V1\SongController;
+use App\Http\Controllers\Api\V1\UserController;
 
 // Route::middleware('auth:sanctum')->group(function(){
 //     Route::get('inside-mware', function(){
@@ -14,5 +15,7 @@ Route::group(['prefix'=>'v1'],function(){
   Route::post('/auth/login',[AuthController::class,'login']);
   Route::get('/users/{id}',[UserController::class,'show']);
   Route::put('/users/{id}',[UserController::class,'update'])->middleware('auth:sanctum');
+  Route::post('/songs',[SongController::class,'store'])->middleware('auth:sanctum');
+  Route::delete('/songs/{id}/{user_id}',[SongController::class,'destroy'])->middleware('auth:sanctum');
   Route::post('/auth/logout',[AuthController::class,'logout'])->middleware('auth:sanctum');
 });

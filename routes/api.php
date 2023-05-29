987f2965dcbe5182ef722106e316dc19f81545ff
GordonOtieno <otienogordon95@gmail.com>
2023-05-29 19:57:29 +0300

    Update routes/api.php

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\V1\AuthController;
 use App\Http\Controllers\Api\V1\SongController;
 use App\Http\Controllers\Api\V1\UserController;
+use App\Http\Controllers\Api\V1\SongsByUserController;
 
 Route::group(['prefix'=>'v1'],function(){
   Route::post('/auth/register',[AuthController::class,'register']);
@@ -12,5 +13,6 @@ Route::group(['prefix'=>'v1'],function(){
   Route::put('/users/{id}',[UserController::class,'update'])->middleware('auth:sanctum');
   Route::post('/songs',[SongController::class,'store'])->middleware('auth:sanctum');
   Route::delete('/songs/{id}/{user_id}',[SongController::class,'destroy'])->middleware('auth:sanctum');
+  Route::get('/user/{user_id}/songs',[SongsByUserController::class,'index'])->middleware('auth:sanctum');
   Route::post('/auth/logout',[AuthController::class,'logout'])->middleware('auth:sanctum');
 });

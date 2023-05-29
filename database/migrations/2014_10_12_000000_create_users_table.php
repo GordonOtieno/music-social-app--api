e2b0fcc0e1be020ff4f5ee689660c71013390d2f
GordonOtieno <otienogordon95@gmail.com>
2023-05-29 19:56:56 +0300

    Update database/migrations/2014_10_12_000000_create_users_table.php file

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,8 +16,8 @@ return new class extends Migration
             $table->string('first_name');
             $table->string('last_name');
             $table->string('email')->unique();
-            $table ->string('image')->nullable();
-            $table->string('description')->nullable();
+            $table ->text('image')->nullable();
+            $table->text('description')->nullable();
             $table->string('location')->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');

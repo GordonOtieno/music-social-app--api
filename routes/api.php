436dff883ae9b2cc390ad88f952a030cd601a8e3
GordonOtieno <otienogordon95@gmail.com>
2023-05-30 20:53:22 +0300

    Update router file, added youtube resourse routes

--- routes/api.php
+++ routes/api.php
@@ -5,12 +5,20 @@ use App\Http\Controllers\Api\V1\AuthController;
 use App\Http\Controllers\Api\V1\SongController;
 use App\Http\Controllers\Api\V1\UserController;
 use App\Http\Controllers\Api\V1\SongsByUserController;
+use App\Http\Controllers\Api\V1\YoutubeController;
+
 
 Route::group(['prefix'=>'v1'],function(){
+
   Route::post('/auth/register',[AuthController::class,'register']);
   Route::post('/auth/login',[AuthController::class,'login']);
   Route::get('/users/{id}',[UserController::class,'show']);
   Route::put('/users/{id}',[UserController::class,'update'])->middleware('auth:sanctum');
+  
+  Route::get('/youtube/{user_id}',[YoutubeController::class,'show'])->middleware('auth:sanctum');
+  Route::post('/youtube',[YoutubeController::class,'store'])->middleware('auth:sanctum');
+  Route::delete('/youtube/{id}',[YoutubeController::class,'destroy'])->middleware('auth:sanctum');
+
   Route::post('/songs',[SongController::class,'store'])->middleware('auth:sanctum');
   Route::delete('/songs/{id}/{user_id}',[SongController::class,'destroy'])->middleware('auth:sanctum');
   Route::get('/user/{user_id}/songs',[SongsByUserController::class,'index'])->middleware('auth:sanctum');
